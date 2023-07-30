584908e9ef727fd1694f191ea12550db30ef52b9
Mohammad Mansur Ali <80177222+mohammedmansurali92@users.noreply.github.com>
2023-07-30 18:10:05 +0300

    LoopUserInputCountFiveTime.js
    This file contains five-time user input the sum

---
+++ LoopUserInputCountFiveTime.js
@@ -0,0 +1,8 @@
+for(var x=1; x<=5; x++){
+    var num1= parseInt(prompt("Enter your first Number"));
+    var num2= parseInt(prompt("Enter Your Second Number"));
+   var sum=num1+num2;
+    document.write("<h1>Result :</h1>"+sum);
+}
+
+ 
